299c8c9955b2323efd45d796ddf39ad7729534e4
sasekazu <sasekazu@gmail.com>
2023-06-15 18:39:26 +0900

    Complete trainer.cpp

--- trainer.cpp
+++ trainer.cpp
@@ -6,38 +6,27 @@ using namespace std;
 using namespace dlib;
 namespace plt = matplotlibcpp;
 
-// typedef matrix<std::vector<int>> sample_type;
 typedef matrix<double,136,1> sample_type;
-// typedef matrix<double,2,1> sample_type;
 
 int main() {
     try{
         TrainingDataRaw data_raw;
         load("out.cereal", data_raw);
-
-        // for(int i=0; i<data_raw.labels.size(); ++i) {
-        //     std::vector<int> x = GetX(data_raw, i);
-        //     std::vector<int> y = GetY(data_raw, i);
-        //     std::vector<int> labels(x.size());
-        //     std::fill(labels.begin(), labels.end(), data_raw.labels[i]);
-        //     plt::scatter_colored(x, y, labels);
-        // }
-        // plt::show();
-
-        typedef one_vs_one_trainer<any_trainer<sample_type> > ovo_trainer;
-        // typedef svm_multiclass_linear_trainer<any_trainer<sample_type> > ovo_trainer;
-        ovo_trainer trainer;
-        
-        typedef radial_basis_kernel<sample_type> rbf_kernel;
-        krr_trainer<rbf_kernel> rbf_trainer;
-        rbf_trainer.set_kernel(rbf_kernel(1.0));
-        trainer.set_trainer(rbf_trainer);
-
         std::vector<sample_type> samples = GetPartsMatrix(data_raw);
         std::vector<double> labels = ConvertToDouble(data_raw.labels);
         cout << "Num data: " << labels.size() << endl;
+
+        typedef linear_kernel<sample_type> lin_kernel;
+        typedef svm_multiclass_linear_trainer <lin_kernel, double> svm_mc_trainer;
+        svm_mc_trainer trainer;        
+        multiclass_linear_decision_function<lin_kernel,double> df = trainer.train(samples,labels);
+
         randomize_samples(samples, labels);
-        cout << "cross validation: \n" << cross_validate_multiclass_trainer(trainer, samples, labels, 10) << endl;
+        trainer.set_c(1); // hyper parameter 
+        cout << "cross validation: " << trainer.get_c() << "\n" <<
+        cross_validate_multiclass_trainer(trainer, samples, labels, 5) << endl;
+
+        serialize("decision_function.dat") << df;
     }
     catch(std::exception& e){
         std::cout << "Error at main(): " << e.what() << std::endl;
